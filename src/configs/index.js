37acbbc278b90e5d65175ee709155b4c8cca0a77
Jason.Zhang <lefttjs@gmail.com>
2016-04-23 19:09:40 +0800

    change api domain

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,11 @@
 /**
  * Created by jason on 4/20/16.
  */
+
+
+//const domain = 'http://localhost:7410'
+const domain = 'http://uphall.lefttjs.com'
+
 export default {
-	domain: 'http://localhost:7410'
+	domain
 }
